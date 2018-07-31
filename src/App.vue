adf28aa050868de8ef019b6d0ca182b27219418d
SpongeK <674018671@qq.com>
2018-07-31 20:24:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <div class="nav">
+      <div class="nav-item"><router-link to="goods">商品</router-link></div>
+      <div class="nav-item"><router-link to="ratings">评价</router-link></div>
+      <div class="nav-item"><router-link to="sellers">商家</router-link></div>
+    </div>
+    <div class="content">
+    </div>
+    <div class="footer">
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/header'
+import axios from 'axios'
+export default {
+  name: 'App',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    'v-header': Header
+  },
+  created () {
+    axios.get('/good/seller').then(res => {
+      if(res.data.code === 0) {
+        this.seller = res.data.data
+      }
+    })
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+}
+.nav {
+  display: flex;
+  justify-content: center;
+  height: 40px;
+  line-height: 40px;
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+  .nav-item{
+    flex-grow: 1;
+    text-align: center;
+    a{
+      color: rgb(77,85,93);
+      font-size: 14px;
+    }
+    &.active a{
+     color: rgb(240, 20, 20);
+    }
+  }
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" alt="" class="set-img" />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div class="supports" v-if="seller.supports">
+          <span class="icon" :class="iconMap[seller.supports[0].type]"></span>
+          <span>{{seller.supports[0].description}}</span>
+        </div>
+        <div class="num-wrapper" v-if="seller.supports">
+          <span class="num">{{seller.supports.length}}个</span>
+          <span class="icon-keyboard_arrow_right more"></span>
+        </div>
+      </div>
+    </div>
+    <div class="bulletin-wrapper">
+    </div>
+    <div class="background">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  created () {
+    this.iconMap = ['decrease', 'discount', 'special', 'invoice_1', 'guarantee']
+  }
+}
+</script>
+
+<style lang="less">
+.content-wrapper{
+  background-color: rgba(7, 17, 27, 0.5);
+  position: relative;
+  .avatar{
+    display:inline-block;
+    margin: 24px 24px 18px;
+    width: 64px;
+    height: 64px;
+    border-radius: 2px;
+    img{
+      width: 100%;
+      height: 100%;
+      border-radius: 2px;
+    }
+  }
+  .content{
+    
+    display: inline-block;
+    color: rgb(255, 255, 255);
+    .title{
+      margin: 2px 4px;
+      height: 18px;
+      .brand{
+        display:inline-block;
+        margin-right: 6px;
+        background-image: url('../assets/images/brand@2x.png');
+        width: 30px;
+        height: 18px;
+        background-size: 30px 18px;
+        vertical-align: top;
+      }
+      .name{
+        font-size: 16px;
+        font-weight: bold;
+        line-height: 18px;
+      }
+    }
+    .description{
+      margin-top: 8px;
+      font-size: 12px;
+      font-weight: 100;
+      height: 12px;
+      line-height: 12px;
+    }
+    .supports{
+      margin-top: 10px;
+      margin-bottom: 2px;
+      font-size: 10px;
+      font-weight: 100;
+      line-height: 16px;
+      height: 16px;
+      .icon{
+        display: inline-block;
+        margin-right: 4px;
+        width: 16px;
+        height: 16px;
+        vertical-align: top;
+        background-size: 16px;
+        &.decrease{
+          background-image: url('../assets/images/decrease_2@2x.png');
+        }
+        &.discount{
+          background-image: url('../assets/images/discount_2@2x.png');
+        }
+        &.guarantee{
+          background-image: url('../assets/images/guarantee_2@2x.png');
+        }
+        &.invoice{
+          background-image: url('../assets/images/invoice_1@2x.png');
+        }
+        &.special{
+          background-image: url('../assets/images/special_1@2x.png');
+        }
+      }
+    }
+    .num-wrapper{
+      position: absolute;
+      right: 12px;
+      bottom: 14px;
+      background-color: rgba(0, 0, 0, 0.2);
+      border-radius: 12px;
+      padding: 7px 8px;
+      font-size: 10px;
+      height: 12px;
+      line-height: 12px;
+    }
+  }
+}
+</style>
